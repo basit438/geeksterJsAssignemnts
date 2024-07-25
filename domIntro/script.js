c9d72138a1c4ba17fdc5d15936cf6cd9a0bf7d01
Basit <basitmanzoor438@gmail.com>
2024-07-25 10:25:38 +0530

    upload geekster dom assignment

---
+++ domIntro/script.js
@@ -0,0 +1,15 @@
+let box = document.getElementById('box');
+
+box.style.backgroundColor ="yellow";
+
+box.style.margin ="20px";
+
+box.style.padding ="18px";
+
+box.style.fontSize ="18px";
+
+box.style.fontWeight ="bold";
+
+box.style.height ="200px";
+
+box.style.width ="300px";
